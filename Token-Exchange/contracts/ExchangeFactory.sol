248bab9f3d80b544b98cefb315cd593a46c8e765
Darkells <zs834916224@gmail.com>
2025-04-04 22:07:22 +0800

    Refer to uniswapV2 core to initialize the contract

---
+++ Token-Exchange/contracts/ExchangeFactory.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./interfaces/IExchangeFactory.sol";
+import "./ExchangePair.sol";
+
+contract ExchangeFactory is IExchangeFactory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    constructor(address _feeToSetter) {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'Exchange: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Exchange: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Exchange: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(ExchangePair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IExchangePair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'Exchange: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, "Exchange: FORBIDDEN");
+        feeToSetter = _feeToSetter;
+    }
+}
